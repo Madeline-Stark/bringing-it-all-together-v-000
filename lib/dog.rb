c6a73fe37141ac5a426eba883573a8d1f65946ee
Madeline Stark <madeline.stark1@gmail.com>
2018-08-17 18:27:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,20 +51,20 @@ end
 #        SQL
 #        DB[:conn].execute(sql, self.name, self.breed, self.id)
 # end
-#
-# def save
-#       if self.id
-#         self.update
-#       else
-#           sql = <<-SQL
-#           INSERT INTO dogs (name, breed)
-#           VALUES (?, ?)
-#           SQL
-#           DB[:conn].execute(sql, self.name, self.breed)
-#           @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-#       end
-# end
-#
+
+def save
+      if self.id
+        self.update
+      else
+          sql = <<-SQL
+          INSERT INTO dogs (name, breed)
+          VALUES (?, ?)
+          SQL
+          DB[:conn].execute(sql, self.name, self.breed)
+          @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      end
+end
+
 # def self.create(name, breed)
 #    dog = self.new(name, breed)
 #    dog.save
