b59025597f5319cb8d456c3e9c727324ad8c7fc1
Madeline Stark <madeline.stark1@gmail.com>
2018-08-17 18:32:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -82,7 +82,7 @@ end
 #     end
 #     dog
 # end
-# 
+#
 # def self.find_by_id(id)
 #   sql = <<-SQL
 #     SELECT *
@@ -90,7 +90,7 @@ end
 #     WHERE id = ?
 #     LIMIT 1
 #   SQL
-# 
+#
 #   DB[:conn].execute(sql, id).map do |row|
 #     self.new_from_db(row)
 #   end.first
