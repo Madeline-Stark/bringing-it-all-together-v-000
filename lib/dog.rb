cf1c5a8dfa8c6a5be4228a4c2af8ec92d86235b5
Madeline Stark <madeline.stark1@gmail.com>
2018-08-17 18:31:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,35 +66,35 @@ def save
       self
 end
 
-# def self.create(name, breed)
-#    dog = self.new(name, breed)
-#    dog.save
-#    dog
-# end
-#
-# def self.find_or_create_by(name:, breed:)
-#     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-#     if !dog.empty?
-#       dog_data = dog[0]
-#       dog = self.new(dog_data[0], dog_data[1], dog_data[2])
-#     else
-#       dog = self.create(name: name, breed: breed)
-#     end
-#     dog
-# end
-#
-# def self.find_by_id(id)
-#   sql = <<-SQL
-#     SELECT *
-#     FROM dogs
-#     WHERE id = ?
-#     LIMIT 1
-#   SQL
-#
-#   DB[:conn].execute(sql, id).map do |row|
-#     self.new_from_db(row)
-#   end.first
-# end
+def self.create(name, breed)
+   dog = self.new(name, breed)
+   dog.save
+   dog
+end
+
+def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = self.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+end
+
+def self.find_by_id(id)
+  sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE id = ?
+    LIMIT 1
+  SQL
+
+  DB[:conn].execute(sql, id).map do |row|
+    self.new_from_db(row)
+  end.first
+end
 
 
 
